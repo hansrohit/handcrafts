93cee23a1e99adb6e52d4a763130bd007593d065
Hans Rohit <hansrohit031@gmail.com>
2024-10-06 16:35:17 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { ShoppingCart } from "lucide-react";
 
 const Navbar = () => {
   return (
     <>
       <div className="h-[3.5rem] rounded-bl-sm rounded-br-sm shadow-md w-full nav flex justify-center items-center border-2">
-        <div className="h-full w-[95%] md:w-[98%] flex justify-between items-start border-2">
+        <div className="h-full w-[99%] md:w-[96%] flex justify-between items-start border-2">
           <div className="h-full w-auto md:w-auto flex justify-center items-center border-2">
             <h1 className="text-xl font-semibold font-serif">
               Crafty Creations
@@ -13,6 +14,12 @@ const Navbar = () => {
           <div className="h-full w-auto md:w-auto md:gap-5 border-2 flex justify-center items-center gap-2">
             <div className="h-full w-auto border-2 flex items-center">
               Products
+            </div>
+            <div className="h-full w-auto border-2 flex items-center">
+              <div>
+                <ShoppingCart size={30} />
+              </div>
+              Cart
             </div>
             <div className="h-full w-auto border-2 flex items-center">
               Login/Signup
